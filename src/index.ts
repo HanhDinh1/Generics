e336f8b2a88af74d4b8fb8c538a335560a1c2552
HanhDinh1 <ms.myhanh89@gmail.com>
2023-11-11 17:22:41 -0500

    write another generic function

--- src/index.ts
+++ src/index.ts
@@ -31,6 +31,15 @@ function identity<T>(item: T): T { // genetic function called identity.
 // identity<string>("hello");
 // identity<Cat>();
 
+function getRandomElement<T>(list: T[]): T {
+  const randIdx = Math.floor(Math.random() * list.length);
+  return list[randIdx];
+}
+
+console.log(getRandomElement<string>(["a", "b", "c"]));
+getRandomElement<number>([5, 6, 21, 345, 756]);
+
+
 // // Providing a type to querySelector:
 // const inputEl = document.querySelector<HTMLInputElement>("#username")!;
 // console.dir(inputEl);
