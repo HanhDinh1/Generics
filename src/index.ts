b02c4563d1fd503b311ef8aa3d0a612abe897710
HanhDinh1 <ms.myhanh89@gmail.com>
2023-11-16 17:21:12 -0500

    complete generics lessons

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 // const nums: Array<number> = [];
 // const colors: Array<string> = [];
 
+//// Providing a type to querySelector:
 // const inputEl = document.querySelector("#username");
 // console.log(inputEl);
 // inputEl.value = "Hacked!";
@@ -26,6 +27,8 @@ interface Cat {
   name: string;
   breed: string;
 }
+
+// With A Generic...Super Reusable!
 function identity<T>(item: T): T { // genetic function called identity.
   return item;
 }
@@ -75,99 +78,24 @@ function makeEmptyArray<T = number>() : T[]{
 const number = makeEmptyArray();
 const bool = makeEmptyArray<boolean>()
 
-// // Providing a type to querySelector:
-// const inputEl = document.querySelector<HTMLInputElement>("#username")!;
-// console.dir(inputEl);
-// inputEl.value = "Hacked!";
+interface Song {
+  title: string;
+  artist: string;
+}
+interface Video {
+  titter: string;
+  creator: string;
+  resolution: string;
+}
 
-// const btn = document.querySelector<HTMLButtonElement>(".btn")!;
+class Playlist <T> {
+  public queue: T[] = [];
+  add(el: T) {
+    this.queue.push(el);
+  }
+}
 
-// // Without Generics...Lots of Repetition!
-// function numberIdentity(item: number): number {
-//   return item;
-// }
-// function stringIdentity(item: string): string {
-//   return item;
-// }
-// function booleanIdentity(item: boolean): boolean {
-//   return item;
-// }
+const songs = new Playlist<Song>();
+const videos = new Playlist<Video>();
+videos.add();
 
-// // function identity(item: any): any{
-// //   return item;
-// // }
-
-// // With A Generic...Super Reusable!
-// function identity<T>(item: T): T {
-//   return item;
-// }
-
-// identity<number>(7);
-// identity<string>("hello");
-
-// function getRandomElement<T>(list: T[]): T {
-//   const randIdx = Math.floor(Math.random() * list.length);
-//   return list[randIdx];
-// }
-
-// console.log(getRandomElement<string>(["a", "b", "c"]));
-// getRandomElement<number>([5, 6, 21, 354, 567, 234, 654]);
-// getRandomElement([1, 2, 3, 4]);
-
-// // Generics With Constraints:
-// function merge<T extends object, U extends object>(object1: T, object2: U) {
-//   return {
-//     ...object1,
-//     ...object2,
-//   };
-// }
-
-// const comboObj = merge({ name: "colt" }, { pets: ["blue", "elton"] });
-// console.log(merge({ name: "Colt" }, { num: 9 }));
-// merge<{ name: string }, { pets: string[] }>(
-//   { name: "colt" },
-//   { pets: ["blue", "elton"] }
-// );
-
-// interface Lengthy {
-//   length: number;
-// }
-
-// function printDoubleLength<T extends Lengthy>(thing: T): number {
-//   return thing.length * 2;
-// }
-
-// // function printDoubleLength(thing: Lengthy): number {
-// //   return thing.length * 2;
-// // }
-
-// printDoubleLength("asdasd");
-// printDoubleLength(234); //Not allowed!
-
-// function makeEmptyArray<T = number>(): T[] {
-//   return [];
-// }
-
-// const nums = makeEmptyArray();
-// const bools = makeEmptyArray<boolean>();
-
-// // A Generic Class Example
-// interface Song {
-//   title: string;
-//   artist: string;
-// }
-// interface Video {
-//   title: string;
-//   creator: string;
-//   resolution: string;
-// }
-
-// class Playlist<T> {
-//   public queue: T[] = [];
-//   add(el: T) {
-//     this.queue.push(el);
-//   }
-// }
-
-// const songs = new Playlist<Song>();
-// const videos = new Playlist<Video>();
